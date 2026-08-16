946f43e8c3a587efe9ea620583920f9098d6b515
Patel-Kingkumar <kingpatel2275479@gmail.com>
2026-08-16 15:29:47 +0530

    use custome css file

--- src/assets/components/Footer.jsx
+++ src/assets/components/Footer.jsx
@@ -1,7 +1,15 @@
-function Footer() { 
+function Footer() {
+    function handleClick() {
+        console.log("Hello World");
+    }
+
     return (
         <div>
             <h1>Footer Component</h1>
+
+            <button className="btn btn-primary" onClick={handleClick}>
+                Click Me
+            </button>
         </div>
     )
 }
--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -1,6 +1,7 @@
+import './Header.css';
 function Header(props) { 
     return (
-        <div>
+        <div className="header">
             <h1>Header Component</h1>
             <p>Welcome, {props.name}!</p>
         </div>
