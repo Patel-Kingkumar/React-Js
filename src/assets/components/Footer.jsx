836d8df8424ed47198abee92953d92f6eaf22695
Patel-Kingkumar <kingpatel2275479@gmail.com>
2026-08-16 17:39:50 +0530

    simple useState

--- src/assets/components/Footer.jsx
+++ src/assets/components/Footer.jsx
@@ -1,12 +1,25 @@
+import { useState } from "react";
+
 function Footer() {
+
+    const [count, setCount] = useState(0);
+
+    // 1. Changes the state
+    //    ↓
+    // 2. Tells React to render again
+    //    ↓
+    // 3.UI gets the new count
+
     function handleClick() {
         console.log("Hello World");
+        setCount(count + 1);
+        console.log("Count: ", count);
     }
 
     return (
         <div className="header">
             <h1>Footer Component</h1>
-
+            <p>Count: { count }</p>
             <button className="btn btn-primary" onClick={handleClick}>
                 Click Me
             </button>
